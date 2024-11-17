84bdd1fc710b256f3fbcae83fe75cffefdec7ea9
Nitish Yadav <yadavnitish00028@gmail.com>
2024-11-17 15:57:22 +0530

    Delete button issue solved

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
 const notesContainer = document.querySelector(".notesContainer");
 const createBtn = document.querySelector(".btn");
-let notes = document.querySelectorAll(".input-box");
 
-function showNotes(){
-    notesContainer.innerHTML = localStorage.getItem("notes");
+function showNotes() {
+    if (localStorage.getItem("notes")) {
+        notesContainer.innerHTML = localStorage.getItem("notes");
+        const noteWrappers = notesContainer.querySelectorAll('.note-wrapper');
+        noteWrappers.forEach(wrapper => {
+            const inputBox = wrapper.querySelector('.input-box');
+            const img = wrapper.querySelector('img');
+            adjustDeleteButtonPosition(inputBox, img);
+            window.addEventListener('resize', () => adjustDeleteButtonPosition(inputBox, img));
+        });
+    }
 }
 
 showNotes();
 
-function updateStorage(){
-    localStorage.setItem("notes" , notesContainer.innerHTML);
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
 }
 
-createBtn.addEventListener("click", ()=>{
+createBtn.addEventListener("click", () => {
+    let noteWrapper = document.createElement('div');
+    noteWrapper.className = 'note-wrapper';
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
-    inputBox.className = "input-box";
-    inputBox.setAttribute("contenteditable" , "true");
-    img.src = "images/delete.png";
-    notesContainer.appendChild(inputBox).appendChild(img);
-})
 
-notesContainer.addEventListener("click", function(e){
-    if (e.target.tagName === "IMG"){
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+    noteWrapper.appendChild(inputBox);
+    noteWrapper.appendChild(img);
+    notesContainer.appendChild(noteWrapper);
+
+    inputBox.addEventListener('input', updateStorage);
+    adjustDeleteButtonPosition(inputBox, img);
+
+    window.addEventListener('resize', () => adjustDeleteButtonPosition(inputBox, img));
+
+    updateStorage();
+});
+
+function adjustDeleteButtonPosition(inputBox, img) {
+    const dimensions = inputBox.getBoundingClientRect();
+    img.style.position = 'absolute';
+    img.style.left = (dimensions.width - 30) + 'px';
+    img.style.bottom = '24px';
+}
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
         updateStorage();
-    }
-    else if (e.target.tagName === "P"){
-        notes = document.querySelectorAll(".input-box");
+    } else if (e.target.tagName === "P") {
+        let notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
-            nt.onkeyup = function(){
+            nt.onkeyup = function () {
                 updateStorage();
-            }
-        })
+            };
+        });
     }
-})
+});
 
-document.addEventListener("keydown" , event => {
-    if (event.key === "Enter"){
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
         document.execCommand("insertLineBreak");
         event.preventDefault();
     }
-})
+});
